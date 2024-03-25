f5efe129f0bddec763ce012c6ea9bbda53a23f9c
s28977 <s28977@pjwstk.edu.pl>
2024-03-25 10:44:39 +0100

    Created ContainerShip class, but tried to test in in Program but it doesn't work bc of generic issues

---
+++ APBD_ContainersApp/APBD_ContainersApp/ContainerShip.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+
+namespace APBD_ContainersApp;
+
+public class ContainerShip
+{
+    private Dictionary<int, AbstractContainer<AbstractCargo>> _containers;
+    private int _weight;
+    public int MaxSpeed { get; }
+    public int MaxContainers { get; }
+    public int MaxWeight { get; }
+
+    public ContainerShip(int maxSpeed, int maxContainers, int maxWeight)
+    {
+        MaxSpeed = maxSpeed;
+        MaxContainers = maxContainers;
+        MaxWeight = maxWeight;
+        _containers = new Dictionary<int, AbstractContainer<AbstractCargo>>();
+        _weight = 0;
+    }
+
+    public bool Load(AbstractContainer<AbstractCargo> container)
+    {
+        if (_weight + container.CargoMass + container.TareWeight > MaxWeight*1000)
+        {
+            Console.WriteLine($"Cannot load the container. Weight of the ship would exceed maximum weight = {MaxWeight}.");
+            return false;
+        }
+
+        if (_containers.Count == MaxContainers)
+        {
+            Console.WriteLine("Cannot load the container. There is no space left on the ship.");
+            return false;
+        }
+        _containers.Add(container.Id, container);
+        return true;
+    }
+    public bool Load(List<AbstractContainer<AbstractCargo>> containers)
+    {
+        var containersMass = 0;
+        foreach (var container in containers)
+        {
+            containersMass += container.CargoMass + container.TareWeight;
+        }
+        if (_weight + containersMass > MaxWeight*1000)
+        {
+            Console.WriteLine($"Cannot load containers. Weight of the ship would exceed maximum weight = {MaxWeight}.");
+            return false;
+        }
+        if (_containers.Count + containers.Count > MaxContainers)
+        {
+            Console.WriteLine("Cannot load containers. There is too little space left on the ship.");
+            return false;
+        }
+        foreach (var container in containers)
+        {
+            _containers.Add(container.Id, container);
+        }
+        return true;
+    }
+
+    public void Remove(int id)
+    {
+        _containers.Remove(id);
+    }
+
+    public void Replace(int id, AbstractContainer<AbstractCargo> container)
+    {
+        Remove(id);
+        Load(container);
+    }
+
+    public void Transfer(int id, ContainerShip ship)
+    {
+        if (ship.Load(_containers[id]))
+        {
+            Remove(id);
+        }
+    }
+
+    public override string ToString()
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append($"Maximum speed: {MaxSpeed} knots, maximum number of containers: {MaxContainers}," +
+                             $" maximum weight of containers: {MaxWeight}").AppendLine();
+        foreach (var container in _containers)
+        {
+            stringBuilder.Append(container.Value).AppendLine();
+        }
+        {
+            
+        }
+        return stringBuilder.ToString();
+    }
+}
--- APBD_ContainersApp/APBD_ContainersApp/Program.cs
+++ APBD_ContainersApp/APBD_ContainersApp/Program.cs
@@ -3,7 +3,11 @@
 public class Program{
     public static void Main(string[] args)
     {
-        AbstractContainer abstractContainer = new AbstractContainer(5);
-        abstractContainer.CargoMass
+        var ship = new ContainerShip(20, 10, 1000);
+        var liquidCargo = new LiquidCargo("Milk", false);
+        var gasContainer = new GasContainer();
+        var liquidContainer = new LiquidContainer();
+        var refrigeratedContainer = new RefrigeratedContainer();
+        ship.Load(gasContainer);
     }
 }
